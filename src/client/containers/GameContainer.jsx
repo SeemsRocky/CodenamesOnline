279504b4b2b75e0848f87069af4885fe0f6ecde8
SeemsRocky <rliao3613@gmail.com>
2020-04-17 14:39:10 -0400

    changed to hooks + functional component

--- src/client/containers/GameContainer.jsx
+++ src/client/containers/GameContainer.jsx
@@ -1,39 +1,25 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import Board from '../components/Board';
 import ChatboxContainer from './ChatboxContainer';
 import ClueDisplay from '../components/ClueDisplay';
 
-const mapStateToProps = (state) => ({
-  sessionID: state.game.sessionID,
-  // newClue: state.game.newClue,
-  currentClue: state.game.currentClue,
-  guessesLeft: state.game.guessesLeft,
-});
-
-class GameContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-    // anymore methods, add here
-  }
-
-  render() {
-    // in the middle of setting up Clue Display. passing down props
-    const { sessionID, guessesLeft, currentClue } = this.props;
-    return (
-      <section className="game-container">
-        <h2>
-          {`Welcome to iSpy! Session: ${sessionID || 'TBD'}`}
-        </h2>
-        <ClueDisplay guesses={guessesLeft} clue={currentClue} />
-        <section id="GameChatContainer">
-          <Board />
-          <ChatboxContainer />
-        </section>
+const GameContainer = () => {
+  const { sessionID } = useSelector((store) => store.game);
+  const { guessesLeft, currentClue } = useSelector((store) => store.clue);
+  return (
+    <section className="game-container">
+      <h2>
+        {`Welcome to iSpy! Session: ${sessionID || 'TBD'}`}
+      </h2>
+      <ClueDisplay guessesLeft={guessesLeft} currentClue={currentClue} />
+      <section id="GameChatContainer">
+        <Board />
+        <ChatboxContainer />
       </section>
-    );
-  }
-}
+    </section>
+  );
+};
 
-export default connect(mapStateToProps, null)(GameContainer);
+
+export default GameContainer;
