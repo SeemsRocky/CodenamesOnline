12a8112257b0763ab67da64464f13d7b528d707c
SeemsRocky <rliao3613@gmail.com>
2020-04-18 19:42:09 -0400

    removing unused vars

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,7 +1,6 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { createSelector } from 'reselect';
 import {
   newMessage,
   updateTeams,
