b834e7e933c7f78a4186b753ea86c060b03fcaa8
SeemsRocky <rliao3613@gmail.com>
2020-04-18 18:43:54 -0400

    refactored to show correct info

--- src/client/components/ClueDisplay.jsx
+++ src/client/components/ClueDisplay.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 
-const ClueDisplay = ({ currentClue, guessesLeft }) => (
-  <section>
-    {
-      currentClue
-        ? <span>{`${currentClue} ${guessesLeft}`}</span>
-        : <> </>
+const ClueDisplay = ({ currentClue, guessesLeft }) => {
+  const chooseClueDisplay = () => {
+    let guesses = 0;
+    if (currentClue) {
+      guesses = guessesLeft === -1 ? 0 : guessesLeft;
     }
-  </section>
-);
+    return (
+      <span>{`${currentClue} ${guesses}`}</span>
+    );
+  };
+
+  return (
+    <section>
+      {
+      currentClue
+        ? chooseClueDisplay()
+        : <> </>
+      }
+    </section>
+  );
+};
 
 export default ClueDisplay;
